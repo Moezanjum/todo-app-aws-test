940aa4c3eb8a878decc8f708dbdb23baf1f9e3c6
Muhammad Ahmed <ahmed495802@gmail.com>
2023-01-07 18:03:57 +0500

    updated test

--- test.js
+++ test.js
@@ -28,13 +28,13 @@ describe('webdriver', () => {
       const title = await driver.getTitle()
       console.log(title)
       assert.equal(title, "Node To Do Application");
-    }, 2000);
+    }, 20000);
 
     it('Clear previous tasks', async () => {
       await driver.findElement(By.css("#btnClr")).click();    
       const added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
       assert.equal(added_tasksUpdated.length, 0);
-    }, 2000);
+    }, 5000);
 
     it('Add new task', async () => {
       const added_tasks = await driver.findElements(By.css(".added-tasks"))
@@ -50,12 +50,12 @@ describe('webdriver', () => {
     it('Verify newely added task', async () => {
       const added_tasks = await driver.findElements(By.css(".added-tasks-input"))
       assert.equal(await added_tasks[added_tasks.length-1].getText(), "New Task");
-    }, 2000);
+    }, 5000);
 
     it('Remove New Added Tasks', async () => {
       await driver.findElement(By.css(".remove-task")).click();
       const added_tasks = await driver.findElements(By.css(".added-tasks-input"))
       assert.equal(added_tasks.length, 0);
-    }, 2000);
+    }, 5000);
   
 });
