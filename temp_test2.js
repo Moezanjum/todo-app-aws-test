2541bfba6c49db27eef21c7f37359550b0696c66
Muhammad Ahmed <ahmed495802@gmail.com>
2023-01-08 16:37:01 +0500

    Updated Tests

---
+++ temp_test2.js
@@ -0,0 +1,61 @@
+const webdriver = require('selenium-webdriver');
+const firefox    = require('selenium-webdriver/firefox');
+const { By } = require('selenium-webdriver');
+const assert = require('assert');
+const screen = {
+  width: 640,
+  height: 480
+};
+
+describe('webdriver', () => {
+    let driver;
+    before(async () => {
+      driver = new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless())
+      .build();
+      console.log('Loading Webpage');
+      await driver.get(`http://localhost:3000`);
+    }, 3000);
+  
+    after(async () => {
+        console.log("\nTests Completed!!\n");
+        await setTimeout(function() {
+            driver.quit();
+          }, 3000);
+    }, 4000);
+  
+    it('test App loads', async () => {
+      console.log("Testing If app loads");
+      const title = await driver.getTitle()
+      console.log(title)
+      assert.equal(title, "Node To Do Application");
+    }, 2000);
+
+    it('Clear previous tasks', async () => {
+      await driver.findElement(By.css("#btnClr")).click();    
+      const added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
+      assert.equal(added_tasksUpdated.length, 0);
+    }, 5000);
+
+    it('Add new task', async () => {
+      const added_tasks = await driver.findElements(By.css(".added-tasks"))
+      const field = await driver.findElement(By.css(".add-task-input"))
+      // console.log(field);
+      await field.sendKeys("New Task");
+      await driver.findElement(By.css(".add-btn")).click();
+      const added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
+      assert.equal(added_tasksUpdated.length, added_tasks.length+1);
+      
+    }, 5000);
+
+    it('Verify newely added task', async () => {
+      const added_tasks = await driver.findElements(By.css(".added-tasks-input"))
+      assert.equal(await added_tasks[added_tasks.length-1].getText(), "New Task");
+    }, 5000);
+
+    it('Remove New Added Tasks', async () => {
+      await driver.findElement(By.css(".remove-task")).click();
+      const added_tasks = await driver.findElements(By.css(".added-tasks-input"))
+      assert.equal(added_tasks.length, 0);
+    }, 5000);
+  
+});
--- test.js
+++ test.js
@@ -1,61 +1,36 @@
 const webdriver = require('selenium-webdriver');
-const firefox    = require('selenium-webdriver/firefox');
+const firefox    = require('selenium-webdriver/chrome');
 const { By } = require('selenium-webdriver');
-const assert = require('assert');
-const screen = {
-  width: 640,
-  height: 480
-};
 
-describe('webdriver', () => {
-    let driver;
-    before(async () => {
-      driver = new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless())
-      .build();
-      console.log('Loading Webpage');
-      await driver.get(`http://localhost:3000`);
-    }, 3000);
+const test = async () => {
+	const driver = new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless()).build();
+	console.log('Loading Webpage');
+	await driver.get(`http://localhost:3000`);
+    
+    console.log("1) Testing If app loads");
+        const title = await driver.getTitle()
+        console.log(title == "Node To Do Application");
   
-    after(async () => {
-        console.log("\nTests Completed!!\n");
-        await setTimeout(function() {
-            driver.quit();
-          }, 3000);
-    }, 4000);
-  
-    it('test App loads', async () => {
-      console.log("Testing If app loads");
-      const title = await driver.getTitle()
-      console.log(title)
-      assert.equal(title, "Node To Do Application");
-    }, 2000);
-
-    it('Clear previous tasks', async () => {
-      await driver.findElement(By.css("#btnClr")).click();    
-      const added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
-      assert.equal(added_tasksUpdated.length, 0);
-    }, 5000);
-
-    it('Add new task', async () => {
-      const added_tasks = await driver.findElements(By.css(".added-tasks"))
-      const field = await driver.findElement(By.css(".add-task-input"))
-      // console.log(field);
-      await field.sendKeys("New Task");
-      await driver.findElement(By.css(".add-btn")).click();
-      const added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
-      assert.equal(added_tasksUpdated.length, added_tasks.length+1);
+    console.log('2) Clear previous tasks')
+        await driver.findElement(By.css("#btnClr")).click();    
+        let added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
+        console.log(added_tasksUpdated.length == 0);
       
-    }, 5000);
+    console.log('3) Add new task')
+        let added_tasks = await driver.findElements(By.css(".added-tasks"))
+        const field = await driver.findElement(By.css(".add-task-input"))
+        await field.sendKeys("New Task");
+        await driver.findElement(By.css(".add-btn")).click();
+        added_tasksUpdated = await driver.findElements(By.css(".added-tasks"))
+        console.log(added_tasksUpdated.length == added_tasks.length+1);
+        
+    console.log('4) Verify newely added task')
+        added_tasks = await driver.findElements(By.css(".added-tasks-input"))
+        console.log(await added_tasks[added_tasks.length-1].getText() == "New Task");
 
-    it('Verify newely added task', async () => {
-      const added_tasks = await driver.findElements(By.css(".added-tasks-input"))
-      assert.equal(await added_tasks[added_tasks.length-1].getText(), "New Task");
-    }, 5000);
-
-    it('Remove New Added Tasks', async () => {
-      await driver.findElement(By.css(".remove-task")).click();
-      const added_tasks = await driver.findElements(By.css(".added-tasks-input"))
-      assert.equal(added_tasks.length, 0);
-    }, 5000);
-  
-});
+    console.log('5) Remove New Added Tasks')
+        await driver.findElement(By.css(".remove-task")).click();
+        added_tasks = await driver.findElements(By.css(".added-tasks-input"))
+        console.log(added_tasks.length == 0);
+}
+test();
